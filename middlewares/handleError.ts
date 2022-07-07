b3f048c4ed7d527038f04dd0a1c73ae1d9d8df8f
Kethlyn <kethlynsaraa@gmail.com>
2022-07-07 14:50:22 -0300

    feat: middleware - errors

--- middlewares/handleError.ts
+++ middlewares/handleError.ts
@@ -2,5 +2,8 @@ import { Request, Response } from "express";
 
 export async function handleError(error, req: Request, res: Response, next) {
     console.log('error', error);
+    if (error.type === "not found") {
+        return res.status(404).send("Not found");
+    }
     return res.status(500).send("Internal Server Error");
 }
